53e5dd5ec13fd1301766dc6f9e5e5c499e4618cc
Anonymous1563 <hf277540625@gmail.com>
2025-03-04 18:41:01 +0000

    commented out debug code in eval.py

--- Enhancement/eval.py
+++ Enhancement/eval.py
@@ -313,16 +313,16 @@ with torch.inference_mode():
                 mc_psnr.append(utils.calculate_psnr(target, mc_pred))
                 mc_ssim.append(utils.calculate_ssim(img_as_ubyte(target), img_as_ubyte(mc_pred)))
 
-        one_rank_list = one_clip_list
-        # one_rank_list = one_niqe_list
-        sorted_one_rank_list = sorted(one_rank_list, reverse=True)
-        best_score = sorted_one_rank_list[-1]
-        # sorted_one_rank_list = sorted_one_rank_list[0::args.num_samples//4]
-        for _i in range(len(sorted_one_rank_list)):
-            _idx2 = one_rank_list.index(sorted_one_rank_list[_i])
-            utils.save_img((os.path.join(result_dir, '{:.2f}.png'.format(sorted_one_rank_list[_i]))), img_as_ubyte(one_pred_list[_idx2]))
+        # one_rank_list = one_clip_list
+        # # one_rank_list = one_niqe_list
+        # sorted_one_rank_list = sorted(one_rank_list, reverse=True)
+        # best_score = sorted_one_rank_list[-1]
+        # # sorted_one_rank_list = sorted_one_rank_list[0::args.num_samples//4]
+        # for _i in range(len(sorted_one_rank_list)):
+        #     _idx2 = one_rank_list.index(sorted_one_rank_list[_i])
+        #     utils.save_img((os.path.join(result_dir, '{:.2f}.png'.format(sorted_one_rank_list[_i]))), img_as_ubyte(one_pred_list[_idx2]))
 
-        # utils.save_img((os.path.join(result_dir, os.path.splitext(os.path.split(inp_path)[-1])[0] + '.png')), img_as_ubyte(best_one_pred))
+        utils.save_img((os.path.join(result_dir, os.path.splitext(os.path.split(inp_path)[-1])[0] + '.png')), img_as_ubyte(best_one_pred))
 
 end_time = time.perf_counter()
 execution_time = end_time - start_time
